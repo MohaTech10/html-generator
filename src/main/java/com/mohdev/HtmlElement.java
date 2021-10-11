2560e194b425476321dd220020f4e76f84485c9b
Mohammed <moody54324@gmail.com>
2021-10-11 23:23:39 +0300

    I should work on it more./createProcess You can add your own tag-attribute using varargs java, And leave everything on HtmlTags! !(:

--- src/main/java/com/mohdev/HtmlElement.java
+++ src/main/java/com/mohdev/HtmlElement.java
@@ -1,26 +1,61 @@
 package com.mohdev;
 
+// TODO: Bad design, Very rigid, Everything depends on __inheritance__
+
 import java.util.ArrayList;
 import java.util.List;
 
 // We should also classify attribute and map to which tags
-class Attribute {
+abstract class Attribute {
+
+    public abstract String represent(HtmlElement htmlElement);
+
 }
+
+class JSReference {
+
+}
+
+class ScriptBlock {
+    JSReference jsReference;
+
+    // public String callName;
+}
+
+class ScriptableAttr extends Attribute {
+    final ScriptBlock  function = null;// FIXME
+
+    void executeJS() {
+        // Execute/call/link block
+    }
+
+    // TODO: Parameter: An attribute is set to a tag, Yes! However the scope of the project can get rid of this injection by -ignoreValid flag , i.e, User responsible for such input
+    @Override
+    public String represent(HtmlElement htmlElement) {
+        return " online=\"" + "scriptFunction()" + ";\"";
+    }
+}
+
+
 class StyleAttr extends Attribute {
     public String value; // FIXME: StyleAttr has property:value;
+
     public StyleAttr() {
     }
 
     public void execute() {
         // TODO: Use REGEX to make sure they're in the right form, i.e,. property: value. And if there's more than one pair make sure separating with `;'
     }
-    public String represent() {
+
+    public String represent(HtmlElement htmlElement) {
         return " style=\"" + value + ";\"";
     }
 }
+
 interface TopLStructureTag {
     void closeOff(StringBuffer buffer);
 }
+
 class HtmlTemplate {
     private final List<HtmlElement> treeNodes;
     private final StringBuffer outputTemplate;
@@ -46,8 +81,9 @@ class HtmlTemplate {
         return new HtmlTemplate();
     }
 
-    public void addToBody(HtmlElement htmlElement) {
+    public HtmlElement addToBody(HtmlElement htmlElement) {
         body.push(htmlElement);
+        return htmlElement;
     }
 
     public void addTopLevelTag(TopLStructureTag topLevelTag) {
@@ -79,13 +115,16 @@ class HtmlTemplate {
     }
 
     // public void addInlineStyle(HtmlElement tag, String cssValue);
-    public void addInlineStyleBody(String value) {
-        body.addStyleValue(value);
+    public void addAttrsBody(Attribute... attributes) {
+        body.addAttrs(attributes);
     }
 }
+
 public abstract class HtmlElement {
     public abstract void generate(StringBuffer buffer);
+    public abstract void setAttributes(Attribute... attrs);
 }
+
 // A group of tags under structural elements
 class StructuralElem extends HtmlElement {
 
@@ -93,16 +132,29 @@ class StructuralElem extends HtmlElement {
     public void generate(StringBuffer buffer) {
 
     }
+
+    @Override
+    public void setAttributes(Attribute... attrs) {
+
+    }
 }
+
 class TextTag extends HtmlElement {
     @Override
     public void generate(StringBuffer buffer) {
 
     }
+
+    @Override
+    public void setAttributes(Attribute... attrs) {
+
+    }
 }
+
 class HeadTag extends StructuralElem implements TopLStructureTag {
 
-    List<HtmlElement> elements;
+    final List<HtmlElement> elements;
+
     private HeadTag() {
         elements = new ArrayList<>();
     }
@@ -111,8 +163,14 @@ class HeadTag extends StructuralElem implements TopLStructureTag {
     public static HeadTag make() {
         return new HeadTag();
     }
-    public void push(HtmlElement htmlElement) { elements.add(htmlElement); }
-    public void acceptTags(List<HtmlElement> tags) { elements.addAll(tags); }
+
+    public void push(HtmlElement htmlElement) {
+        elements.add(htmlElement);
+    }
+
+    public void acceptTags(List<HtmlElement> tags) {
+        elements.addAll(tags);
+    }
 
     @Override
     public void generate(StringBuffer buffer) {
@@ -126,36 +184,53 @@ class HeadTag extends StructuralElem implements TopLStructureTag {
 }
 
 class Body extends StructuralElem implements TopLStructureTag {
-    List<HtmlElement> elements;
-    StyleAttr styleAttr = new StyleAttr();  // Default
+    final List<HtmlElement> elements;
+    final List<Attribute> attributes;
+    final StyleAttr styleAttr = new StyleAttr();  // Default
+
     private Body() {
         elements = new ArrayList<>();
+        attributes = new ArrayList<>();
     }
 
     public static Body make() {
         return new Body();
     }
-    public void push(HtmlElement htmlElement) { elements.add(htmlElement); }
-    public void acceptTags(List<HtmlElement> tags) { elements.addAll(tags); }
-//    public void addAttribute()
-    public void addStyleValue(String value) {
-        styleAttr.value = value;
+
+    public void push(HtmlElement htmlElement) {
+        elements.add(htmlElement);
     }
+
+    public void acceptTags(List<HtmlElement> tags) {
+        elements.addAll(tags);
+    }
+
+    //    public void addAttribute()
+    public void addAttr(Attribute attribute) {
+        attributes.add(attribute);
+    }
+
+    public void addAttrs(Attribute[] attrs) {
+        attributes.addAll(List.of(attrs));
+    }
+
     @Override
     public void generate(StringBuffer buffer) {
-        if (styleAttr.value == null)
-            buffer.append("<body>\n");
-        else
-            buffer.append("<body").append(styleAttr.represent()).append(">\n"); // TODO: Validate weather styleValue is even valid, i.e, property/value; standard;
+        buffer.append("<body");
+        for (var attr : attributes)
+            buffer.append(attr.represent(this));
+        // attr.execute.tag.repAppend(" stype
+        buffer.append(">\n");
         for (var nestedTag : elements) {
             System.out.println("Getting called?");
             nestedTag.generate(buffer);
         }
     }
+
     public static void main(String[] args) {
         var template = HtmlTemplate.make();
-        template.addInlineStyleBody("color: red");
-        template.addToBody(Paragraph.make("myContent"));
+        template.addAttrsBody(new ScriptableAttr());
+        template.addToBody(Paragraph.make("myContent")).setAttributes(new StyleAttr()); // FIXME: Use Builder pattern. ..().buildAttr(..)
         template.generateTemplate(false);
         /*
         Result <!DOCTYPE html> <html> <head> </head> <body> <p> myContent </p> </body> </html>
@@ -172,14 +247,20 @@ class Body extends StructuralElem implements TopLStructureTag {
 
 class Paragraph extends TextTag {
     private final String content;
-
+    private final List<Attribute> attributes;
     private Paragraph(String content) {
         // TODO: move logic out of standard constructor
         this.content = content;
+        attributes = new ArrayList<>();
     }
 
     public static Paragraph make(String content) {
         return new Paragraph(content);
+    }
+
+    @Override
+    public void setAttributes(Attribute... attrs) {
+        attributes.addAll(List.of(attrs));
     }
 
     @Override
