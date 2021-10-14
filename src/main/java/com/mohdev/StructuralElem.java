958ddde0b2ea4f65d3ab365070ececb86a8859b6
Mohammed <moody54324@gmail.com>
2021-10-14 13:18:26 +0300

    Pull all class declaration to their own public file

---
+++ src/main/java/com/mohdev/StructuralElem.java
@@ -0,0 +1,16 @@
+package com.mohdev;
+
+// A group of tags under structural elements
+public class StructuralElem extends HtmlElement {
+
+    @Override
+    public void generate(StringBuffer buffer) {
+
+    }
+
+
+    @Override
+    public void setAttributes(Attr... attrs) {
+
+    }
+}
